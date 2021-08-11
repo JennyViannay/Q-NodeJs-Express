2df1bd2bb72a0c5599fd6658f5b8b60d5f742ebd
JennyViannay <jenny.viannay75@gmail.com>
2021-08-11 10:34:08 +0200

    Express 03 - post de données

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const connection = require("./db-config");
-
 const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get("/api/movies", (request, response) => {
     connection.query('SELECT * FROM movies', (err, result) => {
@@ -11,6 +16,23 @@ app.get("/api/movies", (request, response) => {
             response.status(200).json(result);
         }
     });
+});
+
+app.post('/api/movies', (req, res) => {
+    console.log(req.body)
+    const { title, director, year, color, duration } = req.body;
+    connection.query(
+        'INSERT INTO movies (title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)',
+        [title, director, year, color, duration],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+                res.status(500).send('Error saving the movie');
+            } else {
+                res.status(201).send('Movie successfully saved');
+            }
+        }
+    );
 });
 
 app.get("/api/users", (request, response) => {
